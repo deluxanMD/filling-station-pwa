6c7c61867f7addb692af9e222959adf15c97b024
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2022-09-17 13:27:13 +0530

    responsive design for text input

--- src/components/text-input/text-input.component.js
+++ src/components/text-input/text-input.component.js
@@ -1,13 +1,21 @@
-import { TextField } from "@mui/material";
 import React from "react";
+import { TextField } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const TextInput = ({ label, value, handleChange }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <TextField
-      fullWidth
       label={label}
       size="small"
-      style={{ backgroundColor: "#ffffff", border: 0 }}
+      style={{
+        backgroundColor: "#ffffff",
+        border: 0,
+        width: isMobile ? "100%" : "35%",
+      }}
       onChange={handleChange}
       value={value}
     />
